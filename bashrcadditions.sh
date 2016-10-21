6fa582bf65bbc42ab8d65a5520bf50f8c86b9a95
TheStrix <parthbhatia98@gmail.com>
2016-10-21 18:20:07 +0530

    yay, no more limits

--- bashrcadditions.sh
+++ bashrcadditions.sh
@@ -53,6 +53,9 @@ alias node3='ssh parth@198.199.71.201'
 alias yufastboot='fastboot -i 0x2A96'
 alias kernelarm64='export ARCH=arm64 && export CROSS_COMPILE=~/cm/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-'
 
+# Gdrive
+alias uploadkenzoCMtoDrive='gdrive upload --parent 0B9JxQ_RHblHkQXBlM1BEcHNjOUU'
+
 # LOGs
 alias kenzologcat='adb shell logcat > ~/Desktop/rn3log/log.txt'
 alias kenzoradiologcat='adb shell logcat -b radio > ~/Desktop/rn3log/radio.txt'
