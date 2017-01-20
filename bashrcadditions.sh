44e9c02309377f3730733eaf4bb3065fe3b3a9f0
TheStrix <parthbhatia98@gmail.com>
2017-01-21 00:44:58 +0530

    update to lineage path

--- bashrcadditions.sh
+++ bashrcadditions.sh
@@ -52,8 +52,8 @@ alias node1='ssh parthb@104.236.248.224'
 alias node3='ssh parth@198.199.71.201'
 alias jelly='ssh thestrix@67.205.162.137'
 alias yufastboot='fastboot -i 0x2A96'
-alias makekernel='make ARCH=arm64 CROSS_COMPILE="/home/parth/cm/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"'
-alias makeoutkernel='make O=../out ARCH=arm64 CROSS_COMPILE="/home/parth/cm/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"'
+alias makekernel='make ARCH=arm64 CROSS_COMPILE="/home/parth/lineage/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"'
+alias makeoutkernel='make O=../out ARCH=arm64 CROSS_COMPILE="/home/parth/lineage/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"'
 alias grepandro='grep -i "\.so\|\.xml\|\.conf\|\.txt\|\.cfg\|\.dat"'
 
 # Gdrive
