d148d7b02605a43098cfa42d108d78b6249f1ed7
TheStrix <parthbhatia@lineageos.org>
2017-04-05 15:42:43 +0530

    Don't install lib32z1-dev
     * lib32z1 is already installed and this conflicts

--- ubuntu16.04.sh
+++ ubuntu16.04.sh
@@ -20,7 +20,7 @@ echo -e "${yellow}Installing Dependencies!${nc}"
 sudo apt update -y
 sudo apt install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush \
-schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32ncurses5-dev \
 gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool figlet \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
