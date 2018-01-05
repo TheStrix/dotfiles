b01486596187419957753dd599b3fd838824f038
TheStrix <parthbhatia98@gmail.com>
2018-01-05 23:00:11 +0530

    dot: I'm not sure why didn't I do this in first place

--- setup/setupdotfiles.sh
+++ setup/setupdotfiles.sh
@@ -5,6 +5,3 @@ for f in ~/.bashrc; do
     [ -e "$f" ] && sed -i "1 i\source ~/.dotfiles/customrcadditions" ~/.bashrc || sudo sed -i "1 i\source ~/customrcadditions" /etc/bash.bashrc
     break
 done
-
-echo "Copying $HOME/.dotfiles/bin/ to $HOME/bin/."
-cp -R $HOME/.dotfiles/bin/* $HOME/bin/.
--- setup/updatedotfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/bin
-shopt -s extglob
-rm !(repo)
-
-cd ~/.dotfiles
-cp -R bin/* $HOME/bin/.
